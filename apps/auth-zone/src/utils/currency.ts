07380e29bd8a48fe6abd97232c1e0656f1a3ff6f
Lucas Alves Costa <50385918+luckasnix@users.noreply.github.com>
2025-08-05 19:29:21 -0300

    Revert "feat: add microfrontend (#13)"
    This reverts commit d2c9aa88059fd09583480eccb9b729e3d2e3395e.

--- apps/auth-zone/src/utils/currency.ts
+++
@@ -1,8 +0,0 @@
-export const formatCurrencyBRL = (valueInCents: number): string => {
-  const value = valueInCents / 100;
-
-  return new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  }).format(value);
-};
---
+++ src/app/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+import { HomeHeader } from "~/components/home-header/home-header";
+import { HomeMain } from "~/components/home-main/home-main";
+import { FooterHome } from "~/components/footer-home/footer-home";
+import { SignInModal } from "~/components/sign-in-modal/sign-in-modal";
+import { SignUpModal } from "~/components/sign-up-modal/sign-up-modal";
+import { useAuthStore } from "~/stores/useAuthStore";
+import { useUserStore } from "~/stores/useUserStore";
+
+import styles from "./page.module.css";
+
+export default function Home() {
+  const router = useRouter();
+  const setUsername = useUserStore((state) => state.setUsername);
+  const setToken = useUserStore((state) => state.setToken);
+  const isSignInModalOpen = useAuthStore((state) => state.isSignInModalOpen);
+  const isSignUpModalOpen = useAuthStore((state) => state.isSignUpModalOpen);
+  const closeSignInModal = useAuthStore((state) => state.closeSignInModal);
+  const closeSignUpModal = useAuthStore((state) => state.closeSignUpModal);
+  const signIn = useAuthStore((state) => state.signIn);
+  const signUp = useAuthStore((state) => state.signUp);
+
+  return (
+    <div className={styles.layout}>
+      <HomeHeader />
+      <HomeMain />
+      <FooterHome />
+      <SignInModal
+        open={isSignInModalOpen}
+        onClose={closeSignInModal}
+        onSignIn={async (value) => {
+          await signIn(value, (response) => {
+            setToken(response.result.token);
+            router.push("/dashboard");
+          });
+        }}
+      />
+      <SignUpModal
+        open={isSignUpModalOpen}
+        onClose={closeSignUpModal}
+        onSignUp={async (value) => {
+          await signUp(value, (response) => {
+            setUsername(response.result.username);
+          });
+        }}
+      />
+    </div>
+  );
+}
