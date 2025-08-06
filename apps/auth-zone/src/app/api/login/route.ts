d05de0d94623bc1b3fed3efb538cf8af59042d3a
Koubatz <47603059+Koubatz@users.noreply.github.com>
2025-08-06 17:02:35 -0300

    feat: add username to auth response (#21)

--- apps/auth-zone/src/app/api/login/route.ts
+++ apps/auth-zone/src/app/api/login/route.ts
@@ -8,7 +8,7 @@ export async function POST(request: Request) {
 
     // Esta função (authUser) faz a chamada para http://backend:8000
     const backendResponse = await authUser({ email, password });
-    const { token } = backendResponse.result;
+    const { token, username } = backendResponse.result;
 
     const serializedCookie = serialize("authToken", token, {
       httpOnly: true,
@@ -18,9 +18,30 @@ export async function POST(request: Request) {
       maxAge: 60 * 60 * 24 * 30, // 30 dias
     });
 
+    const serializedUsername = serialize("username", username, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 30, // 30 dias
+    });
+
+    const serializedEmail = serialize("email", email, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 30, // 30 dias
+    });
+
     return NextResponse.json(
       { message: "Login bem-sucedido!" },
-      { status: 200, headers: { "Set-Cookie": serializedCookie } },
+      {
+        status: 200,
+        headers: {
+          "Set-Cookie": `${serializedCookie}, ${serializedUsername}, ${serializedEmail}`,
+        },
+      },
     );
   } catch (error) {
     console.error("Erro na API Route /api/login:", error);
